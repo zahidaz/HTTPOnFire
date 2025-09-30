a3ccbc3d13054be8117382b0e9a4bc3881724234
zahidaz <me.az.zahid@gmail.com>
2025-09-30 13:55:09 -0700

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.azzahid.hof"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.0.3+${versionCode}"
+        versionCode = 4
+        versionName = "0.0.4+${versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
